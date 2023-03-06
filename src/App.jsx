1fef472ac9e60e0bfc7d3032358e785a88d381bc
Alek30k <alemank23k@gmail.com>
2023-03-06 15:15:19 -0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import Contact from "./components/Contact";
+import Hero from "./components/Hero";
+import Who from "./components/Who";
+import Works from "./components/Works";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return <div className="App">Hello word!</div>;
+  return (
+    <div className="App">
+      <Hero />
+      <Who />
+      <Works />
+      <Contact />
+    </div>
+  );
 }
 
 export default App;
