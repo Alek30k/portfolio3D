a5df6d94f689d40aa146559e685d7a0ac5b71f62
Alek30k <alemank23k@gmail.com>
2023-03-08 15:16:26 -0300

    last commit

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -1,7 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+import { Canvas } from "@react-three/fiber";
+import {
+  OrbitControls,
+  PerspectiveCamera,
+  RenderTexture,
+  Text,
+} from "@react-three/drei";
+
+const Container = styled.div`
+  height: 100vh;
+  width: 100%;
+  scroll-snap-align: center;
+`;
 
 const Test = () => {
-  return <div>Test</div>;
+  return (
+    <Container>
+      <Canvas>
+        <OrbitControls enableZoom={false} />
+        <ambientLight intensity={1} />
+        <directionalLight position={[3, 2, 1]} />
+        <mesh>
+          <boxGeometry args={[2, 2, 2]} />
+          <meshStandardMaterial>
+            <RenderTexture attach="map">
+              <PerspectiveCamera makeDefault position={[0, 0, 2]} />
+              <color attach="background" args={["#dc9dcd"]} />
+              <Text fontSize={1} color="#555">
+                Ale
+              </Text>
+            </RenderTexture>
+          </meshStandardMaterial>
+        </mesh>
+      </Canvas>
+    </Container>
+  );
 };
 
 export default Test;
