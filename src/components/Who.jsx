93d76ff1b220a4294b6b8f551a27b7e7ba973e51
Alek30k <alemank23k@gmail.com>
2023-03-08 15:40:17 -0300

    last commit

--- src/components/Who.jsx
+++ src/components/Who.jsx
@@ -80,14 +80,14 @@ const Who = () => {
     <Section>
       <Container>
         <Left>
-          {/* <Canvas camera={{ position: [5, 5, 5], fov: 25 }}>
+          <Canvas camera={{ position: [5, 5, 5], fov: 25 }}>
             <Suspense fallback={null}>
               <ambientLight intensity={0.5} />
               <directionalLight position={[3, 2, 1]} />
               <Cube />
               <OrbitControls enableZoom={false} autoRotate />
             </Suspense>
-          </Canvas> */}
+          </Canvas>
         </Left>
         <Right>
           <Title>Think outside the square space</Title>
