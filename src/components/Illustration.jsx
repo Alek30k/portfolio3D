1edef12ab3970569914d5f8948dac1b1a4d3ec83
Alek30k <alemank23k@gmail.com>
2023-03-15 18:25:05 -0300

    last commit

--- src/components/Illustration.jsx
+++ src/components/Illustration.jsx
@@ -1,4 +1,4 @@
-import React, { Suspense } from "react";
+import React, { Suspense, useState } from "react";
 import { OrbitControls, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import styled from "styled-components";
@@ -23,7 +23,75 @@ const Desc = styled.div`
   }
 `;
 
+const PortfolioItem = styled.div`
+  flex: 0 0 40.33%;
+  max-width: 40.33%;
+  margin-bottom: 30px;
+  border-radius: 10px;
+`;
+const Inner = styled.div`
+  border-radius: 10px;
+  overflow: hidden;
+  position: relative;
+  cursor: pointer;
+`;
+const InnerImg = styled.div``;
+
+const Item = styled.img`
+  width: 100%;
+  height: 200px;
+  display: block;
+  transition: transform 0.5s;
+
+  &:hover {
+    transform: scale(1.1);
+  }
+`;
+const Layer = styled.div`
+  width: 100%;
+  height: 30%;
+  background: linear-gradient(rgba(0, 0, 0, 0.6), rgb(211, 20, 52));
+  position: absolute;
+  color: #fff;
+  left: 0;
+  bottom: 0;
+  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  text-align: center;
+  transition: height 0.5s;
+`;
+
+const Portfolio = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  margin-left: -15px;
+  margin-right: -15px;
+  position: relative;
+  gap: 10px;
+`;
+const Title = styled.h2``;
+const Enlace = styled.a``;
+
+const Button = styled.button`
+  background-color: #da4ea2;
+  color: white;
+  font-weight: 500;
+  width: 120px;
+  padding: 10px;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  position: absolute;
+  bottom: 500px;
+  right: 100px;
+`;
+
 const Illustration = () => {
+  const [portfolioOpen, setPortfolioOpen] = useState(false);
+
   return (
     <>
       {portfolioOpen ? (
