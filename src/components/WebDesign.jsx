c3d1328d988cc2933859eab72230beefc7dafddc
Alek30k <alemank23k@gmail.com>
2023-03-15 23:15:39 -0300

    last commit

--- src/components/WebDesign.jsx
+++ src/components/WebDesign.jsx
@@ -2,7 +2,7 @@ import { OrbitControls, Stage } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import React, { Suspense } from "react";
 import styled from "styled-components";
-import Mac_laptop from "./Mac_laptop";
+import Mac from "./Mac";
 
 const Desc = styled.div`
   width: 200px;
@@ -13,7 +13,6 @@ const Desc = styled.div`
   position: absolute;
   top: 100px;
   right: 100px;
-
   @media only screen and (max-width: 768px) {
     top: 0;
     bottom: 0;
@@ -29,9 +28,9 @@ const WebDesign = () => {
       <Canvas>
         <Suspense fallback={null}>
           <Stage environment="city" intensity={0.6}>
-            <Mac_laptop />
+            <Mac />
           </Stage>
-          <OrbitControls enableZoom={true} autoRotate />
+          <OrbitControls enableZoom={false} autoRotate />
         </Suspense>
       </Canvas>
       <Desc>
