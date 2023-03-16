bc109d76cdf1ba4a0536ae7a68e351a58e69f2c6
Alek30k <alemank23k@gmail.com>
2023-03-15 23:15:22 -0300

    last commit

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,53 +1,16 @@
 import React, { useState } from "react";
 import styled from "styled-components";
 import Development from "./Development";
-import Illustration from "./Illustration";
 import ProductDesign from "./ProductDesign";
 import WebDesign from "./WebDesign";
-import SocialMedia from "./SocialMedia";
 
 const data = [
   "Web Design",
-  "Ecommerce",
-  "Social Media",
   "Development",
-  "ProductDesign",
+  "Illustration",
+  "Product Design",
+  "Social Media",
 ];
-
-const Works = () => {
-  const [work, setWork] = useState("Web Design");
-
-  return (
-    <Section id="works">
-      <Container>
-        <Left>
-          <List>
-            {data.map((item) => (
-              <ListItem key={item} text={item} onClick={() => setWork(item)}>
-                {item}
-              </ListItem>
-            ))}
-          </List>
-        </Left>
-        <Right>
-          {work === "Web Design" ? (
-            <WebDesign />
-          ) : work === "Development" ? (
-            <Development />
-          ) : work === "ProductDesign" ? (
-            <ProductDesign />
-          ) : work === "Ecommerce" ? (
-            <Illustration />
-          ) : (
-            <SocialMedia />
-          )}
-        </Right>
-      </Container>
-    </Section>
-  );
-};
-
-export default Works;
 
 const Section = styled.div`
   height: 100vh;
@@ -64,7 +27,6 @@ const Container = styled.div`
   width: 1400px;
   display: flex;
   justify-content: space-between;
-
   @media only screen and (max-width: 768px) {
     width: 100%;
     flex-direction: column;
@@ -75,7 +37,6 @@ const Left = styled.div`
   flex: 1;
   display: flex;
   align-items: center;
-
   @media only screen and (max-width: 768px) {
     padding: 20px;
     justify-content: center;
@@ -96,13 +57,11 @@ const ListItem = styled.li`
   color: transparent;
   -webkit-text-stroke: 1px white;
   position: relative;
-
   @media only screen and (max-width: 768px) {
     font-size: 24px;
     color: white;
     -webkit-text-stroke: 0px;
   }
-
   ::after {
     content: "${(props) => props.text}";
     position: absolute;
@@ -113,7 +72,6 @@ const ListItem = styled.li`
     overflow: hidden;
     white-space: nowrap;
   }
-
   &:hover {
     ::after {
       animation: moveText 0.5s linear both;
@@ -128,8 +86,34 @@ const ListItem = styled.li`
 
 const Right = styled.div`
   flex: 1;
-  width: 60%;
-  height: 60%;
-  margin-left: 80px;
-  margin-top: 180px;
 `;
+
+const Works = () => {
+  const [work, setWork] = useState("Web Design");
+  return (
+    <Section>
+      <Container>
+        <Left>
+          <List>
+            {data.map((item) => (
+              <ListItem key={item} text={item} onClick={() => setWork(item)}>
+                {item}
+              </ListItem>
+            ))}
+          </List>
+        </Left>
+        <Right>
+          {work === "Web Design" ? (
+            <WebDesign />
+          ) : work === "Development" ? (
+            <Development />
+          ) : (
+            <ProductDesign />
+          )}
+        </Right>
+      </Container>
+    </Section>
+  );
+};
+
+export default Works;
